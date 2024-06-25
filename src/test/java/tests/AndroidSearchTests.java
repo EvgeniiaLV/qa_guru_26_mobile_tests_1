2367d56203f2a28a674d3058bfea3a64ae5e8199
Evgeniia Liasheva <evgenia_lozovik@mail.ru>
2024-06-25 13:09:10 +0300

    Added tags

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -11,6 +12,7 @@ import static io.qameta.allure.Allure.step;
 
 public class AndroidSearchTests extends TestBase {
 
+    @Tag("android")
     @Test
     void successfulSearchTest() {
         step("Type search", () -> {
